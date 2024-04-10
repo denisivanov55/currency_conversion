06691b289b76d4a2094f8663e9f8c34f7dba4977
Denis Ivanov <divan090386@gmail.com>
2024-04-10 09:19:28 +0600

    Add currency converter

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+    url: "https://v6.exchangerate-api.com/v6/07504bb84f70b78170708f45",
+    codes: [],
+    pair: {
+      from: "",
+      to: "",
+    },
+    amount: "",
+    loading: false,
+    currentTab: "convert",
+    currency: {
+      code: "USD",
+    },
+    currencies: ["USD", "EUR", "BYN"],
+    actions: {
+      remove: "remove",
+      change: "change",
+    },
+  };
